22e61b96a4d2dbec848c9066b3536be3ac321768
Trần Đăng Khôi <baits16@gmail.com>
2023-08-16 16:06:20 +0700

    :+1: Re-add react query as async state management

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./styles/index.scss";
 import "react-toastify/dist/ReactToastify.css";
 import "sweetalert2/src/sweetalert2.scss";
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <ToastContainer />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+      <ToastContainer />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
