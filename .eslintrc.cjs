5d73fb5c2dcfcb41b82408e36473a18321f3d4c1
Trần Đăng Khôi <baits16@gmail.com>
2023-08-17 10:41:28 +0700

    :wrench: Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,6 +31,9 @@ module.exports = {
     },
   },
   rules: {
+    // Tắt rule yêu cầu import React trong file jsx
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "prettier/prettier": [
       "warn",
