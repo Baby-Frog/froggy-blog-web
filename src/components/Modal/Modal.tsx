cf93b2a2aa92b7898366f3c9358c0c0aee632e95
Trần Đăng Khôi <baits16@gmail.com>
2023-08-31 16:39:17 +0700

    :+1: Update close icon width/height

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -51,8 +51,8 @@ const ModalCloseButton = styled.button`
   justify-content: center;
   align-items: center;
   svg {
-    width: 12px;
-    height: 12px;
+    width: 10px;
+    height: 10px;
   }
 `;
 
