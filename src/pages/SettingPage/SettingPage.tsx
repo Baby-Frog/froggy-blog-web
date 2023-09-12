4dd437b24f66fb67fd74a976590d5ef8abeb7f33
Trần Đăng Khôi <baits16@gmail.com>
2023-09-12 13:18:20 +0700

    :+1: Create bunch of pages

---
+++ src/pages/SettingPage/SettingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type TSettingPageProps = {
+  something: string;
+};
+
+const SettingPage = () => {
+  return <div></div>;
+};
+
+export default SettingPage;
---
+++ src/pages/UserProfilePage/UserProfilePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type TUserProfilePageProps = {
+  something: string;
+};
+
+const UserProfilePage = () => {
+  return <div></div>;
+};
+
+export default UserProfilePage;
