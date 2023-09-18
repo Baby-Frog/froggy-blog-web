7e8a3be0868df97499110f4631f83514bc3de482
TranDangKhoi <baits16@gmail.com>
2023-09-18 20:10:00 +0700

    :nail_care: Update loading page on mobile

--- src/pages/LoadingPage/LoadingPage.tsx
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import LoadingGIF from "src/assets/loading.gif";
+import { useMedia } from "react-use";
 import Logo from "src/assets/logo-4.png";
 type TLoadingPageProps = {
   children?: React.ReactNode;
@@ -7,6 +8,7 @@ type TLoadingPageProps = {
 
 const LoadingPage = ({ children }: TLoadingPageProps) => {
   const [dotAnimation, setDotAnimation] = useState<string>(".");
+  const isMobile = useMedia("(max-width: 767px)");
   useEffect(() => {
     const interval = setInterval(() => {
       setDotAnimation((prev) => {
@@ -25,7 +27,7 @@ const LoadingPage = ({ children }: TLoadingPageProps) => {
             alt="Logo"
             className="w-[50px] h-[50px]"
           />
-          <h2 className="text-[32px] tracking-[-2px] font-bold">Froggy Blog</h2>
+          <h2 className="md:text-[32px] text-[24px] tracking-[-2px] font-bold">Froggy Blog</h2>
         </div>
       </div>
       <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
@@ -36,10 +38,17 @@ const LoadingPage = ({ children }: TLoadingPageProps) => {
             width={380}
             height={380}
           />
-          <div className="text-lg flex items-center">
-            {children}
-            <span className="w-[25px]">{dotAnimation}</span>
-          </div>
+          {isMobile ? (
+            <div className="text-center">
+              <span className="text-sm">{children}</span>
+              <span className="w-[25px] text-sm">{dotAnimation}</span>
+            </div>
+          ) : (
+            <div className="md:text-lg flex md:items-center text-sm items-end">
+              <span>{children}</span>
+              <span className="w-[25px]">{dotAnimation}</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
