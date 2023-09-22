0064ba2a284aa387663b838d251979f2f6336a6b
Trần Đăng Khôi <baits16@gmail.com>
2023-09-22 14:02:09 +0700

    :wrench: Fix logout bug

--- src/apis/auth.apis.ts
+++ src/apis/auth.apis.ts
@@ -6,6 +6,6 @@ export const authApi = {
   register: (body: { email: string; password: string; rePassword: string }) =>
     http.post<TAuthResponse>(ENDPOINTS.REGISTER, body),
   login: (body: { email: string; password: string }) => http.post<TAuthResponse>(ENDPOINTS.LOGIN, body),
-  logout: (body: { refreshToken: string }) => http.post(`/api${ENDPOINTS.LOGOUT}`, body),
+  logout: (body: { refreshToken: string }) => http.post(ENDPOINTS.LOGOUT, body),
   refreshToken: (body: { refreshToken: string }) => http.post<TAuthResponse>(ENDPOINTS.REFRESH_TOKEN, body),
 };
