73e03bf978b699c1803841d63170b69595c8632d
Trần Đăng Khôi <baits16@gmail.com>
2023-09-26 16:46:03 +0700

    :wrench: Update env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_LOCAL_API_URL: string;
   readonly VITE_STAGING_API_URL: string;
   readonly VITE_TINY_MCE_API_KEY: string;
-  readonly VITE_TINY_MCE_PRIVATE_KEY: string;
-  readonly VITE_TINY_MCE_PUBLIC_KEY: string;
   readonly VITE_IBB_IMAGE_UPLOAD_API_URL: string;
   // more env variables...
 }
