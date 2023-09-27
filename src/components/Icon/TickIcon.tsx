7808e15431e2f90f2f22860fa5a669792de7be64
Trần Đăng Khôi <baits16@gmail.com>
2023-09-27 10:30:16 +0700

    :wrench: Fix errors in console

--- src/components/Icon/TickIcon.tsx
+++ src/components/Icon/TickIcon.tsx
@@ -11,9 +11,9 @@ const TickIcon = (props: JSX.IntrinsicElements["svg"]) => {
       <path
         d="M4 11.5455L8.95226 16.3289C9.33979 16.7033 9.95421 16.7033 10.3417 16.3289L20 7"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
