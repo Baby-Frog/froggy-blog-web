77f3f69a7c8ffa0b6b36d0b72c467ed866afe7b0
Trần Đăng Khôi <baits16@gmail.com>
2023-09-29 09:41:11 +0700

    :muscle: Update schemas

--- src/schemas/story.schemas.ts
+++ src/schemas/story.schemas.ts
@@ -3,13 +3,16 @@ import * as yup from "yup";
 export const storySchema = yup.object({
   title: yup
     .string()
-    .required("Title is required")
+    .required("Please enter a title")
     .min(6, "Title must be at least 6 characters long")
     .max(160, "Title must be at most 160 characters long"),
-  content: yup.string().required("Content is required").min(30, "Content must be at least 30 characters long"),
+  content: yup
+    .string()
+    .required("Please write something about your story")
+    .min(30, "Your story is too short, it must be at least 30 characters long"),
   // thumbnail: yup.string().required("Thumbnail is required"),
   // topicId is an array with string type, and it must not []
-  topicId: yup.array().of(yup.string()).required("Topic is required").min(1, "Topic is required"),
+  topicId: yup.array().of(yup.string()).required("Please choose your topics").min(1, "Please choose your topics"),
   // Thumbnail should be an image, and is not required
   thumbnail: yup.string(),
   credit: yup.string(),
