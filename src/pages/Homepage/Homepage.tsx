28cf9f2ea8d30ba7384dbaad4d1e869cc656c2af
Trần Đăng Khôi <baits16@gmail.com>
2023-10-03 17:27:34 +0700

    :+1: Update icons

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,12 +1,5 @@
-import { useIsPresent } from "framer-motion";
-import PageTransition from "src/components/PageTransition";
 const Homepage = () => {
-  const isPresent = useIsPresent();
-  return (
-    <>
-      <PageTransition isPresent={isPresent}></PageTransition>
-    </>
-  );
+  return <></>;
 };
 
 export default Homepage;
