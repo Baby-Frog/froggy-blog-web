828add1b4cc9bb4df272a6700a523c70ff5d2bdf
Trần Đăng Khôi <baits16@gmail.com>
2023-10-03 11:24:40 +0700

    :+1: Config yarn dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dns from "dns";
 import path from "path";
-
-dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   plugins: [react()],
