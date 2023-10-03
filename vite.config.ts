129ad8cb02e8670f6fae0feed153207db8bc5ab6
Trần Đăng Khôi <baits16@gmail.com>
2023-10-03 10:19:59 +0700

    :wrench: Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dns from "dns";
 import path from "path";
+
+dns.setDefaultResultOrder("verbatim");
+
 export default defineConfig({
   plugins: [react()],
   server: {
