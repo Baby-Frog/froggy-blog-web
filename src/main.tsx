5d9d6d7be7b1ab9f9836211798a9dd0f07668321
TranDangKhoi <baits16@gmail.com>
2023-10-06 23:31:31 +0700

    :dizzy_face: Update some minor but important stuffs

--- src/main.tsx
+++ src/main.tsx
@@ -17,8 +17,6 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnMount: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
     },
   },
 });
