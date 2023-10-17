a43c5992031a05ae88e9871beb98fddf668e9c76
TranDangKhoi <baits16@gmail.com>
2023-10-17 22:35:14 +0700

    ✅ Allow more characters in bio

--- src/schemas/profile.schema.ts
+++ src/schemas/profile.schema.ts
@@ -9,7 +9,7 @@ export const profileSchema = yup.object({
   address: yup.string().max(160, "Address must be at most 160 characters long"),
   birthDay: yup.string(),
   phoneNumber: yup.string().max(20, "Phone number must be at most 20 characters long"),
-  bio: yup.string().max(160, "Bio must be at most 160 characters long"),
+  bio: yup.string().max(220, "Bio must be at most 220 characters long"),
 });
 
 export type TProfileSchema = yup.InferType<typeof profileSchema>;
