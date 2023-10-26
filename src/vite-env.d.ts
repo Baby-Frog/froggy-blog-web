30aa8ca61316a0253e28c02ec1ae62d55c6052b8
Trần Đăng Khôi <baits16@gmail.com>
2023-10-26 10:37:28 +0700

    :100: Update .env.example

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,5 @@ interface ImportMetaEnv {
   readonly VITE_RECAPTCHA_SITE_KEY: string;
   readonly VITE_IBB_IMAGE_UPLOAD_API_URL: string;
   readonly VITE_ENV: "PROD" | "LOCAL";
-  readonly VITE_RECAPTCHA_SITE_KEY: string;
   // more env variables...
 }
