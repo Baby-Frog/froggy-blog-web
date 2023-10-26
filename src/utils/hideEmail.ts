dc9c06e6ad4611952743b1ce1a60057c9299a5ea
TranDangKhoi <baits16@gmail.com>
2023-10-26 23:20:31 +0700

    :wrench: Update hideEmail util to make it more secured

--- src/utils/hideEmail.ts
+++ src/utils/hideEmail.ts
@@ -1,5 +1,5 @@
 export const hideEmail = (email?: string) => {
-  if (email && email.split("@")[0].length <= 3) return email?.replace(/(\w{1})[\w.-]+@([\w.]+\w)/, "$1•••••@$2");
+  if (email && email.split("@")[0].length <= 4) return email?.replace(/(\w{1})[\w.-]+@([\w.]+\w)/, "$1•••••@$2");
   const partialEmail = email?.replace(/(\w{3})[\w.-]+@([\w.]+\w)/, "$1•••••@$2");
   return partialEmail;
 };
