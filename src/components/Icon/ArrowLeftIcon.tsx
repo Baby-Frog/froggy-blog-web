90fffdf2f6ef407a16614ecf5c21ca3592c20f98
TranDangKhoi <baits16@gmail.com>
2023-10-28 23:40:08 +0700

    :+1: Modify icons

--- src/components/Icon/ArrowLeftIcon.tsx
+++ src/components/Icon/ArrowLeftIcon.tsx
@@ -5,10 +5,12 @@ const ArrowLeftIcon = (props: JSX.IntrinsicElements["svg"]) => {
       height={26}
       viewBox="0 0 19 19"
       aria-hidden="true"
+      fill="currentColor"
       {...props}
     >
       <path
         d="M11.47 13.97L6.99 9.48 11.47 5l.55.5-3.99 3.98 4 4z"
+        fill="currentColor"
         fillRule="evenodd"
       />
     </svg>
