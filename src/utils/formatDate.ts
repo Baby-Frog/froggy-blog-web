2c711dd0a22e76d9cb2117ffdf05b9db9b50a12b
TranDangKhoi <baits16@gmail.com>
2023-10-29 17:39:03 +0700

    :calendar: Update date utils

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,9 +4,39 @@ export const getMonthName = (date: Date) => {
   return monthNames[date.getMonth()];
 };
 
+export const getFullMonthName = (date: Date) => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return monthNames[date.getMonth()];
+};
+
 export const getCustomDate = (date: Date) => {
   const month = getMonthName(date);
   const day = date.getDate();
   const year = date.getFullYear() === new Date().getFullYear() ? "" : date.getFullYear();
   return `${month} ${day} ${year}`;
+};
+
+export const getCustomDateByString = (date: string) => {
+  const newDate = new Date(date);
+  const month = getFullMonthName(newDate);
+  const day = newDate.getDate();
+  return `${month} ${day} `;
+};
+
+export const getDateTime = (date: string) => {
+  return new Date(date).toLocaleDateString("en-GB");
 };
